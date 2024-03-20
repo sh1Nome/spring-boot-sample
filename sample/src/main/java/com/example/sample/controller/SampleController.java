1732f5b8b85a277a47c3791f111a090fad576e53
sh1Nome <shinodayo1024@gmail.com>
2024-03-20 10:15:35 +0000

    [update]コントローラ分割

--- sample/src/main/java/com/example/sample/controller/SampleController.java
+++ sample/src/main/java/com/example/sample/controller/SampleController.java
@@ -1,24 +1,15 @@
 package com.example.sample.controller;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.example.sample.entity.Hello;
 import com.example.sample.form.IndexForm;
-import com.example.sample.repository.HelloCrudRepository;
 
 @Controller
 public class SampleController {
-    @Autowired
-    HelloCrudRepository repository;
 
     @ModelAttribute
     public IndexForm setUpForm() {
@@ -28,22 +19,6 @@ public class SampleController {
     @RequestMapping(value="index", method=RequestMethod.GET)
     public String dispatchIndex(Model model) {
         return "index";
-    }
-
-    @RequestMapping(value="hello", method=RequestMethod.POST)
-    public String dispatchHello(@Validated IndexForm form, BindingResult bindingResult, Model model) {
-        if (bindingResult.hasErrors()) {
-            return "index";
-        }
-
-        Optional<Hello> word = repository.findById(1);
-        if(word.isPresent()) {
-            String hello = word.get().getWord();
-            model.addAttribute("hello", hello);
-            return "hello";
-        }
-
-        return "err";
     }
     
 }
