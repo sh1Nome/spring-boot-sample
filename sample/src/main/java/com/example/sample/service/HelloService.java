20d691b017d1ae96302bdcbcd56a225191aa2387
sh1Nome <shinodayo1024@gmail.com>
2024-03-21 12:19:55 +0000

    [update]中途半端、仮

---
+++ sample/src/main/java/com/example/sample/service/HelloService.java
@@ -0,0 +1,5 @@
+package com.example.sample.service;
+
+public interface HelloService {
+
+}
---
+++ sample/src/main/java/com/example/sample/service/HelloServiceImpl.java
@@ -0,0 +1,35 @@
+package com.example.sample.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+
+import com.example.sample.entity.Hello;
+import com.example.sample.repository.HelloCrudRepository;
+
+import lombok.Data;
+
+@Data
+public class HelloServiceImpl implements HelloService {
+
+    Integer id = null;
+
+    Model model = null;
+
+    @Autowired
+    HelloCrudRepository repository;
+
+    public Optional<Hello> findById() {
+        return repository.findById(this.id);
+    }
+
+    public void ifPresentAddAttribute() {
+        Optional<Hello> entitys = this.findById();
+        entitys.ifPresent((e) -> {
+
+        });
+    }
+
+    public void addAttribute()
+}
