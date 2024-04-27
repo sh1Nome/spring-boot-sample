8f4b5dd34735f6cad459e7f0ce199e53d44bc9b4
sh1Nome <shinodayo1024@gmail.com>
2024-04-28 01:12:23 +0900

    update

--- pegination/src/main/java/com/example/pegination/service/ServerSideServiceImpl.java
+++ pegination/src/main/java/com/example/pegination/service/ServerSideServiceImpl.java
@@ -3,7 +3,6 @@ package com.example.pegination.service;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.pegination.entity.Hoge;
